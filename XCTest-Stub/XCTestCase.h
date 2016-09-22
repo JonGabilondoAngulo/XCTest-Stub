5711d2c7cccc93aa022fc5d5a40b45b92a40b542
Jon Gabilondo <jon.gabilondo.angulo@gmail.com>
2016-09-22 12:46:09 +0300

    Implementations.

--- XCTest-Stub/XCTestCase.h
+++ XCTest-Stub/XCTestCase.h
@@ -4,11 +4,11 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import <XCTest/XCTest.h>
+#import "XCTest.h"
 
 @class NSInvocation, XCTestCaseRun, XCTestContext, _XCTestCaseImplementation;
 
-@interface XCTestCase ()
+@interface XCTestCase : NSObject 
 /*{
     id _internalImplementation;
 }
--- XCTest-Stub/XCTestSuite.h
+++ XCTest-Stub/XCTestSuite.h
@@ -4,7 +4,7 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import <XCTest/XCTest.h>
+#import "XCTest.h"
 
 @class NSArray, NSMutableArray, NSString;
 
