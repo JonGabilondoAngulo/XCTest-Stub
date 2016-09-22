c3de8a17cb9dde11b709c359d62cfc4be085e592
Jon Gabilondo <jon.gabilondo.angulo@gmail.com>
2016-09-22 11:38:02 +0300

    Project creation

---
+++ XCTest-Stub/XCTest_Stub.h
@@ -0,0 +1,13 @@
+//
+//  XCTest_Stub.h
+//  XCTest-Stub
+//
+//  Created by Jon Gabilondo on 22/09/2016.
+//  Copyright © 2016 organismo-mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCTest_Stub : NSObject
+
+@end
---
+++ XCTest-Stub/XCTest_Stub.m
@@ -0,0 +1,13 @@
+//
+//  XCTest_Stub.m
+//  XCTest-Stub
+//
+//  Created by Jon Gabilondo on 22/09/2016.
+//  Copyright © 2016 organismo-mobile. All rights reserved.
+//
+
+#import "XCTest_Stub.h"
+
+@implementation XCTest_Stub
+
+@end
